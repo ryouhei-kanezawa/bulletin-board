7f386169b537d04563439cf08f5843a381063d6e
ryouhei-kanezawa <k.ryouhei1414@gmail.com>
2021-11-17 15:57:46 +0900

    js

--- client/js/main.js
+++ client/js/main.js
@@ -39,12 +39,58 @@ $('#post').on('click', () => {
 });
 
 
-$('.card .edit').on('click', () => {
-    // TODO: 編集が押された時の処理
-    alert('編集が押されました');
-});
-
-$('.card .delete').on('click', () => {
-    // TODO: 削除が押された時の処理
-    alert('削除が押されました');
-});
+$('.lists').on('click', '.edit', function() {
+    // 投稿id、タイトルとメッセージを取得
+    let $id = $(this).parents('.card').data('id');
+    let message = $(this).parents('.right').prev().text();
+    let title = $(this).parents('.button-block').next().text()
+  
+    // タイトルとメッセージを格納
+    $('#title').val(title);
+    $('#message').val(message);
+  
+    // ボタンテキストを「編集」に変更
+    // classに'update'を追加
+    $('#post').text('更新');
+    $('#post').addClass('update');
+  
+    // 更新を押したら、更新APIを叩く
+    $('.update').on('click', function() {
+      // 更新APIを叩く
+      $.ajax({
+        type: 'PUT',
+        url: url,
+        data: {
+          "id": $id,
+          "title": title,
+          "message": message
+        }
+      }).done(function(data){
+        alert('更新完了しました。');
+        window.location.reload();
+      });
+    });
+  });
+  
+  $('.lists').on('click', '.delete', function() {
+    // 投稿idを取得
+    let $id = $(this).parents('.card').data('id');
+  
+    // アラートを出す
+    let result = window.confirm('本当に削除してよいですか？');
+  
+    if (result) {
+      // 投稿idをもとに削除APIを叩く
+      $.ajax({
+        type: 'POST',
+        url: url + '/delete',
+        data: {
+          "id": $id
+        }
+      }).done(function(data){
+        alert('削除完了しました。');
+        window.location.reload();
+      });
+    }
+  
+  });
